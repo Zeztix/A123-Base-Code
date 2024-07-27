37f03195b2c411eeb7c3a48974733859d521b72b
Aurora Howard <howal002@mymail.unisa.edu.au>
2024-07-27 17:17:06 +0930

    Initial Commit

---
+++ Assign1.java
@@ -0,0 +1,169 @@
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Assign1 {
+
+    public static void main(String[] args) {
+
+	System.out.println("Starting 2D engine...");
+        //Buffer, Canvas and Frame containers.
+        FrameBuffer buffer = null;
+        MainCanvas canvas = null;
+        JFrame frame = null;
+
+        //Tools to read command line and text file instructions
+        BufferedReader reader = null;
+        Scanner scanner;
+        String command;
+        int lineNumber = 0;
+        String trimmedLine;
+        
+        //Look at each file
+        for (int i = 0; i < args.length; i++) {
+            try {
+
+                //Open file and read line
+                reader = new BufferedReader(new FileReader(args[i]));
+                String line = reader.readLine();
+
+                //Process and read every line
+                while (line != null) {
+                	
+                	//Clean up any extra spaces etc.
+                    trimmedLine = line;
+                    while (trimmedLine != null && trimmedLine.trim().equals("")) {
+                        line = reader.readLine();
+                        trimmedLine = line;
+                    }
+                    lineNumber++;
+                    
+                    //ignore empty
+                    if(line == null){
+                        break;
+                    }
+
+                    //Filter comments
+                    if (line.contains("#")) {
+                        String noComments;
+                        noComments = line.substring(0, line.indexOf("#"));
+                        //if there is text before comment, process it, otherwise ignore the line
+                        if (noComments.length() > 0) {
+                            line = noComments;
+                        } else {
+                            line = reader.readLine();
+                            continue;
+                        }
+                    }
+
+                    if (line.contains("(")) {
+                        //strip spaces from after opening bracket - ensure scanner reads RGB values together with next()
+                        line = line.substring(0, line.indexOf("(")) + line.substring(line.indexOf("("), line.length()).replace(" ", "");
+                    }
+
+                    //Store command
+                    command = line.substring(0, line.indexOf(" "));
+
+                    //Read all the arguments into a scanner
+                    scanner = new Scanner(line.substring(line.indexOf(" "), line.length()));
+
+                    switch (command) {
+                        case "INIT":
+                            //Create the buffer and canvas
+                            int width = scanner.nextInt();
+                            int height = scanner.nextInt();
+                            buffer = new FrameBuffer(width, height);
+                            canvas = new MainCanvas(buffer);
+
+                            //Set up a swing frame
+                            frame = new JFrame();
+                            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                            frame.setSize(width, height);
+                            frame.add(canvas);
+                            frame.setTitle("Assign1");
+                            frame.setVisible(true);
+
+                            System.out.println("Creating canvas, frame and buffer of size " + width + "x" + height);
+                            break;
+
+                        case "POINT":
+                            //Get x and y location
+                            int xc = scanner.nextInt();
+                            int yc = scanner.nextInt();
+
+                            //TO DO: need to extract the colours here
+                            //get the colour
+                            String color = scanner.next();
+                            //System.out.println(color);
+                            int red = 255, green=0, blue = 0;
+
+
+                            //Draw a point with our point method
+                            buffer.point(xc, yc, red, green, blue);
+                            canvas.repaint();
+
+                            System.out.println("Drawing point: (" + xc + "," + yc + "R=" + red + " G="+green + " B="+blue + ")");
+                            
+                            break;
+                        case "LINE_FLOAT":
+
+                            //read end point values from next 4 ints
+                            int lnflt_x1 = scanner.nextInt();
+                            int lnflt_y1 = scanner.nextInt();
+                            int lnflt_x2 = scanner.nextInt();
+                            int lnflt_y2 = scanner.nextInt();
+
+                            //int red = 255, green = 0, blue = 0;
+
+                            //TODO: call lineFloat method to change pixel array and update canvas
+                            canvas.repaint();
+
+                            break;
+                            
+                        case "LINE":
+                            break;
+                        case "OUTLINE_POLYGON":
+                            break;
+                        case "FILL_POLYGON":
+                            break;
+                        case "OUTLINE_CIRCLE":
+                            break;
+                        case "FILL_CIRCLE":
+                            break;
+                        case "INVERT":
+                        	break;
+                        case "PAUSE":
+                            int millis = scanner.nextInt();
+                            System.out.println("Pause: " + millis + " milliseconds");
+                            try {
+                                Thread.sleep(millis);
+                            } catch (InterruptedException e) {
+                                System.out.println("Problem with sleep...");
+                                e.printStackTrace();
+                            }
+                            break;
+                        case "SAVE":
+                            String fileName = scanner.next();
+                            canvas.save(fileName);
+                            System.out.println("Saving file:" + fileName);
+
+                            break;
+                        default:
+                            System.out.println("Unknown command at line:  " + lineNumber);
+                    }
+                    line = reader.readLine();
+
+                }
+
+            } catch (FileNotFoundException e) {
+                System.out.println("Error: File could not be found: " + e.getMessage());
+            } catch (IOException e) {
+                System.out.println("Error: Unexpected IO exception encountered");
+            }
+
+        }
+    }
+}
---
+++ FrameBuffer.java
@@ -0,0 +1,54 @@
+/**
+ * Frame buffer class skeleton
+ */
+
+public class FrameBuffer {
+
+	//Store all pixel data in this int array.
+	//NOTE: 1 int should contain red, green and blue data NOT 3!
+	private int[] pixels;
+	private int width;
+	private int height;
+
+	//Set up memory for pixel data
+	public FrameBuffer(int width, int height) {
+		this.width = width;
+		this.height = height;
+		this.pixels = new int[width * height];
+	}
+
+	//A start on the point function. NOTE this is not complete!
+	public void point(int xc, int yc, int r, int g, int b) {
+		// TODO: Add point into array
+		
+	}
+	
+	// Implement other drawing functions here...
+
+	// Definitions for the getRed, getGreen and getBlue functions. NOTE these are not complete!
+	public int getRed(int xc, int yc) {
+		// TODO: Implement these functions using bitwise operations and masking to retrieve individual colour components
+
+		return 0;
+	}
+
+	public int getGreen(int xc, int yc) {
+		return 0;
+	}
+
+	public int getBlue(int xc, int yc) {
+		return 0;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public int[] getPixels() {
+		return pixels;
+	}
+}
